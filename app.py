c6b38d7d713d3841d6739a7b503b4a32141b9ea4
Hemraj saini <hrdynordx@gmail.com>
2026-07-12 14:24:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,38 +37,3 @@ if st.button("Predict"):
 
         else:
             st.error("Negative Review")
-
-from preprocessing import preprocess
-
-# Load Model
-model = joblib.load("models/model.pkl")
-tfidf = joblib.load("models/tfidf.pkl")
-
-st.set_page_config(
-    page_title="Movie Sentiment Analysis"
-)
-
-st.title(" IMDb Movie Review Sentiment Analysis")
-
-st.write("Enter a movie review and predict whether it is Positive or Negative.")
-
-review = st.text_area("Movie Review")
-
-if st.button("Predict"):
-
-    if review.strip() == "":
-        st.warning("Please enter a review.")
-
-    else:
-
-        review = preprocess(review)
-
-        X = tfidf.transform([review])
-
-        prediction = model.predict(X)[0]
-
-        if prediction == 1:
-            st.success(" Positive Review")
-
-        else:
-            st.error("Negative Review")
